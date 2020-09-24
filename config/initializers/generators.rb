dcc4bdbdfee18aca3e33087f130e57d8f8d05f95
yuta.nishioka <yuta914.makenew@gmail.com>
2020-09-24 03:05:12 +0000

    top page

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
 Rails.application.config.generators do |g|
-    g.stylesheets fales
-    g.javascripts fales
-    g.helper fales
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
     g.skip_routes true
 end
